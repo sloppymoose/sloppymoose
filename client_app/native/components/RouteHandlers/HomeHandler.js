2350d5abccf107d5d990b5c54bf6c7d00debd681
Derek Lindahl <derek.lindahl@gmail.com>
2015-12-21 20:39:03 -0800

    Extract HomeScreen from HomeContainer
    This allows the handler to be react-native agnostic

--- client_app/native/components/RouteHandlers/HomeHandler.js
+++ client_app/native/components/RouteHandlers/HomeHandler.js
@@ -1,19 +1,9 @@
-import { Actions } from 'react-native-redux-router';
 import { bindActionCreators } from 'redux';
-import { Component, PropTypes, StyleSheet, Text, View } from 'react-native';
+import { Component, PropTypes } from 'react-native';
 import { connect } from 'react-redux/native';
-import emptyObj from 'empty/object';
-import { SignOutButton } from '../Buttons';
 import { signOutUser } from '../../../react/actions/UserActions';
 
-const baseStyles = StyleSheet.create({
-  root: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-});
+import { HomeScreen } from '../Screens';
 
 function getState(state) {
   return {
@@ -25,42 +15,23 @@ function getActions(dispatch) {
   return bindActionCreators({ signOutUser }, dispatch);
 }
 
-export class _HomeHandler extends Component {
-  componentWillMount() {
-    if(!this.props.user.signedIn) {
-      Actions.signedOut();
-    }
-  }
+export class HomeContainer extends Component {
   render() {
-    if(!this.props.user.signedIn) {
-      return null;
-    }
-    const expiresAt = ((this.props.user.createdAt * 1000) + this.props.user.expiresIn);
-    const timeLeft = this.props.user.expiresIn * 1000 - (Date.now() - expiresAt);
     return (
-      <View style={baseStyles.root}>
-        <Text>Sloppy Moose</Text>
-        <Text>Welcome User!</Text>
-        <Text>Expires In: {this.props.user.expiresIn}</Text>
-        <Text>Created At: {this.props.user.createdAt * 1000}</Text>
-        <Text>Expires At: {expiresAt}</Text>
-        <Text>------Now: {Date.now()}</Text>
-        <Text>TTL: T-{timeLeft / 1000}s</Text>
-        <SignOutButton signOutUser={this.props.signOutUser}/>
-      </View>
+      <HomeScreen
+        signedOut={this.props.signedOut}
+        user={this.props.user}
+      />
     );
   }
 }
 
-_HomeHandler.propTypes = {
+HomeContainer.propTypes = {
   signedOut: PropTypes.bool,
   user: PropTypes.shape({
     accessToken: PropTypes.string,
     signedIn: PropTypes.bool
   })
 };
-_HomeHandler.defaultProps = {
-  user: emptyObj
-};
 
-export const HomeHandler = connect(getState, getActions)(_HomeHandler);
+export const HomeHandler = connect(getState, getActions)(HomeContainer);
---
+++ client_app/native/components/Screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import { Component, PropTypes, StyleSheet, Text, View } from 'react-native';
+import emptyObj from 'empty/object';
+import { SignOutButton } from '../Buttons';
+import { signedRequest } from '../../../react/util/networkHelpers';
+
+const baseStyles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+});
+
+export class HomeScreen extends Component {
+  componentWillMount() {
+    console.info('HomeScreen#componentWillMount');
+    // signedRequest('/api/check_ins')
+    //   .then((data) => {
+    //     console.info('CHECKINS', data);
+    //   })
+    //   .catch(err => {
+    //     console.info('CHECKIN ERROR', err);
+    //   });
+  }
+  render() {
+    console.info('render home handler');
+    if(!this.props.user.signedIn) {
+      return null;
+    }
+    const expiresAt = ((this.props.user.createdAt * 1000) + this.props.user.expiresIn);
+    const timeLeft = this.props.user.expiresIn * 1000 - (Date.now() - expiresAt);
+    return (
+      <View style={baseStyles.root}>
+        <Text>Sloppy Moose</Text>
+        <Text>Welcome User!</Text>
+        <Text>Expires In: {this.props.user.expiresIn}</Text>
+        <Text>Created At: {this.props.user.createdAt * 1000}</Text>
+        <Text>Expires At: {expiresAt}</Text>
+        <Text>------Now: {Date.now()}</Text>
+        <Text>TTL: T-{timeLeft / 1000}s</Text>
+        <SignOutButton signOutUser={this.props.signOutUser}/>
+      </View>
+    );
+  }
+}
+
+HomeScreen.propTypes = {
+  signedOut: PropTypes.bool,
+  user: PropTypes.shape({
+    accessToken: PropTypes.string,
+    signedIn: PropTypes.bool
+  })
+};
+HomeScreen.defaultProps = {
+  user: emptyObj
+};
+
