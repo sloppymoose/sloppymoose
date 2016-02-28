72d0c75d6028f113c601af099051311167072848
Derek Lindahl <derek.lindahl@gmail.com>
2016-02-27 16:14:57 -0800

    Add production Redis configuration

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: Rails.application.secrets.redis_url
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url: Rails.application.secrets.redis_url
+  }
+end
