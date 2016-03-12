11ca46e9806f86660bbbd8ab5927fcadcbee1c13
Derek Lindahl <derek.lindahl@gmail.com>
2016-03-11 22:02:31 -0800

    Add sidekiq configuration YAML
    The free Redis instance available on Heroku did not like the default config

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec rails server puma -b ${HOST:-0.0.0.0} -p ${PORT:-7000} -e ${RACK_ENV:-development}
-worker: bundle exec sidekiq -q default -q mailer
+worker: bundle exec sidekiq -C config/sidekiq.yml
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,15 @@
+# Calculated from http://manuel.manuelles.nl/sidekiq-heroku-redis-calc/
+
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: Rails.application.secrets.redis_url
+    url: Rails.application.secrets.redis_url,
+    size: 9
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: Rails.application.secrets.redis_url
+    url: Rails.application.secrets.redis_url,
+    size: 1
   }
 end
