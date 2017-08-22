2ce53f4de02b3073abfe87d28a7b17c913a9bd35
Derek Lindahl <derek.lindahl@gmail.com>
2017-08-22 12:58:46 -0700

    Add ESLint and prettier

---
+++ sloppymoose-react-native/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'standard',
+    'plugin:import/errors',
+    'plugin:import/react',
+    'plugin:import/warnings',
+    'plugin:react/recommended'
+  ],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  plugins: [],
+  rules: {}
+}
